d7f5c896e96f1d7a44bdfe1bbbdef8002a19457d
DKnox <dknox@redhat.com>
2013-02-25 16:51:19 -0700

    First commit with Maven artifacts

---
+++ src/main/java/org/knoxious/ling_geo/App.java
@@ -0,0 +1,13 @@
+package org.knoxious.ling_geo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
